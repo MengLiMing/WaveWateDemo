4df96d0faf03bfb26a7d3c81b7900a66d336f760
MengLiMing <920459250@qq.com>
2016-11-18 09:08:17 +0800

    Initial Commit

---
+++ WaveWateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaveWateDemo
+//
+//  Created by my on 2016/11/18.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaveWateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaveWateDemo
+//
+//  Created by my on 2016/11/18.
+//  Copyright © 2016年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
